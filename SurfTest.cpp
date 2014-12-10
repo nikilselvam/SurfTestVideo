137806ad8a3935f892734b61429d816ddb167bf2
unknown <npselvam@EN4085063.fulton.ad.asu.edu>
2014-12-10 16:43:34 -0700

    Tested new features for SURF.

--- SurfTest.cpp
+++ SurfTest.cpp
@@ -5,6 +5,7 @@
 #include "opencv2/nonfree/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 
 using namespace cv;
 
